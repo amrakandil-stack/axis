2d72a912fa92d1736694c84471d2196dd6a29b9a
amrakandil-stack <amr.a.kandil@gmail.com>
2026-01-18 16:25:00 +0400

    Create App.tsx
    Add App with routing

---
+++ src/App.tsx
@@ -0,0 +1,25 @@
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Services from "./pages/Services";
+import Contact from "./pages/Contact";
+
+export default function App() {
+  return (
+    <BrowserRouter>
+      <nav style={{ padding: "20px", display: "flex", gap: "15px" }}>
+        <Link to="/">Home</Link>
+        <Link to="/about">About</Link>
+        <Link to="/services">Services</Link>
+        <Link to="/contact">Contact</Link>
+      </nav>
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/services" element={<Services />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
